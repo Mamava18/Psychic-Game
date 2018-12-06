633e5d485df20628cf84bae75e2ada46bbdda060
Mamava18 <44853413+Mamava18@users.noreply.github.com>
2018-12-05 21:10:05 -0500

    Updating my code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,9 @@
 var numwins = 0;
 var numloses = 0;
 var guessleft = 10;
-var compoption = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var userentry = [];
+var compoption = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//var useroption = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //User has to make a choice
 //Computer has to make a choice
 //Compare user and computer choice and determine if win or lose
@@ -12,21 +13,53 @@ var compoption = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 //If win, increase the number of wins until the number of lose is equal to 0
 //Record all the letters guessed by user
 
-document.onkeyup = function() {
-    var letguess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(letguess);
-    var compguess = compoption[Math.floor(Math.random()*compoption.length)];
-    console.log(compguess);
- 
-      if (letguess === compguess) {
-          numwins++;
-          console.log(numwins);
-      }
-      else {
-          numloses++;
-          console.log(numloses);
-       }
-   
+
+document.onkeyup = function (event) {
+
+    var letguess = event.key;
+    console.log("user guesss:" + letguess + ", ");
+    //var letguess = String.fromCharCode(event.keyCode).toLowerCase();
+    //console.log('your guess: ' + letguess);
+    var compguess = compoption[Math.floor(Math.random() * compoption.length)];
+    console.log('computer guess: ' + compguess);
+
+
+    guessleft--;
+    console.log("Guesses left " + guessleft);
+
+    if (guessleft === 0) {
+        numloses++;
+        console.log("Loses: " + numloses);
+        guessleft = 10;
+
+    }
+
+    if (letguess === compguess) {
+        numwins++;
+        console.log("Wins: " + numwins);
+        guessleft = 10;
+        userentry = [];
+        //userentry.push(letguess);
+        console.log("Your choices: " + userentry);
+    }
+    else {
+        //letguess = [];
+        if (numloses >= 1) {
+            userentry.push(letguess);
+            console.log("Your choices: " + userentry);
+        }
+        //numloses++;
+        //guessleft--;
+        //console.log("Guesses left " + guessleft);
+        //userentry = [];
+        //console.log("Loses: " + numloses);
+        //console.log("Your entries: " + userentry);
+    }
+
+
+
+
 }
+
 
 
